ce9a83f6fd55ea8784b3256904e1b4d3b4df0958
Preksha Dahal <prekshadahal88@gmail.com>
2023-12-29 10:54:56 +0545

    Task1 SetUp

---
+++ TreeLeafTechnologies/TreeLeafTechnologies/Task1/JokesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JokesViewController.swift
+//  TreeLeafTechnologies
+//
+//  Created by Preksha Dahal on 29/12/2023.
+//
+
+import UIKit
+
+class JokesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
